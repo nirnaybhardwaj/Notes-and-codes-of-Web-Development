eb5763236136859e6ac79be79d7b8bd7a052c488
nirnaybhardwaj <nirnay99bhardwaj@gmail.com>
2022-05-07 22:35:38 +0530

    first commit

--- Java Script/array.js
+++ Java Script/array.js
@@ -8,17 +8,17 @@ let arr = []
 arr = [1, 2, 3, 4, 'pepcoding', 6.89, 'w']
 
 // Array is just an indexed collection of elements in JS
-// console.log(arr)
+console.log(arr)
 
 
 // access the particular index
-// let test = arr[4]
-// console.log(test)
+let test = arr[4]
+console.log(test)
 
 
 //change any value of n array
-// arr[3] = 'nirnay'
-// console.log(arr)
+arr[3] = 'nirnay'
+
 
 
 // Array Methods
@@ -28,17 +28,17 @@ arr = [1, 2, 3, 4, 'pepcoding', 6.89, 'w']
 //  1. push method
 // push method adds an element of your choice after the last index element 
 
-// arr.push('b')
-// console.log(arr)// last element = b
+arr.push('b')
+console.log(arr)// last element = b
 
 
 //  2. pop method
 // pop method delete an element of last index 
 
-// arr.pop()
-// arr.pop()
-// arr.pop()
-// console.log(arr) 
+arr.pop()
+arr.pop()
+arr.pop()
+console.log(arr) 
 
 
 //  3. unshift method
