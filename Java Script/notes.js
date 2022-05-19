a646cf9d8cc9d514466c544357b8bc344bd26d81
nirnaybhardwaj <nirnay99bhardwaj@gmail.com>
2022-05-19 17:33:41 +0530

    First commit

---
+++ Java Script/notes.js
@@ -0,0 +1,3 @@
+// Q. -  Java Script is a Single - Threaded Synchronous Language. Why?
+
+// Ans. - It performs as a Single Thread (program) and execute synchronously (one by one top to bottom).
--- NodeJS/node_packages/3_fs.js
+++ NodeJS/node_packages/3_fs.js
@@ -4,33 +4,82 @@
 // Delete
 
 
+        // FILES //
+
+
 // fs = File System Module 
 const fs = require('fs')
 
 
-// 1. File ko Read kaise kre (Reading A File)
+// // 1. File ko Read kaise kre (Reading A File)
 
-let content = fs.readFileSync('f1.txt')
-// console.log('Data ' + content)
-//    or
-console.log(content.toString())
-
-
-// 2. Writing into a file
-
-fs.writeFileSync('f2.txt', 'This data will be written on f2 file')
-
-// if the file name is passed doesnot exist a new file will be created with its name and the data will be written on that file
-fs.writeFileSync('f3.txt', 'This will be written in f3 file')
+// let content = fs.readFileSync('f1.txt')
+// // console.log('Data ' + content)
+// //    or
+// console.log(content.toString())
 
 
-// 3. Append(update a file)
+// // 2. Writing into a file
 
-// appendFilesync method  adds new data to a previously written file 
-fs.appendFileSync('f3.txt', ' This is updated data')
+// fs.writeFileSync('f2.txt', 'This data will be written on f2 file')
+
+// // if the file name is passed doesnot exist a new file will be created with its name and the data will be written on that file
+// fs.writeFileSync('f3.txt', 'This will be written in f3 file')
 
 
-// 4. Delete
+// // 3. Append(update a file)
 
-fs.unlinkSync('f1.txt')
-console.log('file deleted')
+// // appendFilesync method  adds new data to a previously written file 
+// fs.appendFileSync('f3.txt', ' This is updated data')
+
+
+// // 4. Delete
+
+// fs.unlinkSync('f1.txt')
+// console.log('file deleted')
+
+
+
+
+
+     // DIRECTORIES OR FOLDERS //
+
+
+// creation of a directory or folders
+//mkdirSync
+
+// fs.mkdirSync('myDirectory')
+// console.log('Directory created')
+// fs.mkdirSync('myDirectory2')
+// console.log('Directory created')
+
+
+// delete a directory or folders
+
+// fs.rmdirSync('myDirectory2')
+// console.log('Directory deleted')
+
+
+// Exists Sync
+// To check whether a directory or file exists or not
+// this returns true or false based on whether the passed directory or file exists or not
+
+// Directory or Folder
+// let doesExist = fs.existsSync('myDirectory3')
+// console.log(doesExist)
+
+//file
+// let doesExist1 = fs.existsSync('f2.txt')
+// console.log(doesExist1)
+
+
+// readdirSync
+
+
+//In Scope
+let folderContent = fs.readdirSync('myDirectory')
+console.log(folderContent)
+
+// Out of scope and double back \ will be added node js rule
+let folderContent1 = fs.readdirSync('D:\\FJP-8\\Java Script')
+console.log(folderContent1)
